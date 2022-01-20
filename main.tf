4d60b5d8e168d3bfabd9ff3eef685053270e5694
kovurusreevarsha <98008861+kovurusreevarsha@users.noreply.github.com>
2022-01-20 22:01:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-region = "us-east-1"
+region = "us-east-2"
 }
 resource "aws_instance" "instance-1" {
-	ami="ami-066157edddaec5e49"
+	ami="ami-001089eb624938d9f"
 	instance_type="t2.micro"
 	count = 1
 	tags = {
-		Name ="EC2_node"
+		Name ="EC2_linux"
 }
 }
 
